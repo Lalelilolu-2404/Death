8271f9324abaade4bb5ff719f7a3f0936dfa3240
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-09-17 03:13:26 +0200

    Update samu.js

--- samu.js
+++ samu.js
@@ -2172,7 +2172,7 @@ break
 
 case 'cami':
 groupx = await samu330.groupMetadata(from)
-mensajeDesc = `Grupo ${groupx.subject}\n${groupx.gid}`
+mensajeDesc = `Grupo ${groupx.subject}\n${groupx.jid}`
 samu330.sendMessage(from, mensajeDesc, MessageType.text)		
 break		
 /**		
